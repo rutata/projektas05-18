543b0ac4d3de4ce57ea1c4b17a27eab573dc0a92
rutata <raganqte@gmail.com>
2017-05-19 10:45:13 +0300

    Pridejau nuotrauku ir papildziau savo puslapius

---
+++ about.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Tinklapis apie keliones kuris keisis</title>
+
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="namudarbas.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+	
+</head>
+<body>
+<header>
+	<!-- cia bus Vytauto sukurtas headeris -->
+</header>
+
+<h2>About us</h2>
+<div class="about-tekstas">
+<p>
+<ul>
+				<li class="about-fontas"><p>The world is a book, and those who do not travel read only a page. Saint Augustine</p></li>
+				<li class="about-fontas"><p>Wherever you go, go with all your heart. Confucius</p></li>
+				<li class="about-fontas"><p>The real voyage of discovery consists not in seeking new landscapes, but in having new eyes. Marcel Proust</p></li>
+				<li class="about-fontas"><p>A good traveler has no fixed plans, and is not intent on arriving. Lao Tzu</p></li>
+				<li class="about-fontas"><p>A journey is a person in itself; no two are alike. And all plans, safeguards, policing, and coercion are fruitless. We find that after years of struggle that we do not take a trip; a trip takes us. John Steinbeck</p></li>
+				<li class="about-fontas"><p>The traveler sees what he sees, the tourist sees what he has come to see. Gilbert K. Chesterton</p></li>
+				<li class="about-fontas"><p>The two impulses in travel are to get away from home, and the other is to pursue something - a landscape, people, an exotic place. Certainly finding a place that you like or discovering something unusual is a very sustaining thing in travel. Paul Theroux
+Read more at: https://www.brainyquote.com/quotes/topics/topic_travel.html</p></li>
+</ul>
+</p>
+</div>
+
+</body>
+</html>
---
+++ singleentry.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Tinklapio title apie keliones kuris keisis</title>
+
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="namudarbas.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+	
+</head>
+<body>
+<header>
+	<!-- cia bus Vytauto sukurtas headeris -->
+</header>
+<div class="foto-fonas"> </div>
+
+<h2>Europa</h2>
+<div class="europa-tekstas">
+<p class="pagrindinis-tekstas">
+Europe is a continent that comprises the westernmost part of Eurasia. Europe is bordered by the Arctic Ocean to the north, the Atlantic Ocean to the west, and the Mediterranean Sea to the south. The eastern boundary with Asia is a historical and cultural construct, as there is no clear physical and geographical separation between them; Europe is generally considered as separated from Asia by the watershed divides of the Ural and Caucasus Mountains, the Ural River, the Caspian and Black Seas, and the waterways of the Turkish Straits. Yet the non-oceanic borders of Europe—a concept dating back to classical antiquity—are arbitrary. The primarily physiographic term "continent" as applied to Europe also incorporates cultural and political elements whose discontinuities are not always reflected by the continent's current overland boundaries.
+
+Europe covers about 10,180,000 square kilometres (3,930,000 sq mi), or 2% of the Earth's surface (6.8% of land area). Politically, Europe is divided into about fifty sovereign states of which the Russian Federation is the largest and most populous, spanning 39% of the continent and comprising 15% of its population. Europe had a total population of about 740 million (about 11% of world population) as of 2015.
+
+The European climate is largely affected by warm Atlantic currents that temper winters and summers on much of the continent, even at latitudes along which the climate in Asia and North America is severe. Further from the sea, seasonal differences are more noticeable than close to the coast.
+</p>
+</div>
+
+<div class="europos-valstybes">
+	<h3 class="h3-pavadinimas">Alphabetical list of countries in Europe you can visit</h3>
+	<p class="valstybiu-sarasas">
+A
+Albania
+Andorra
+Armenia
+Austria
+Azerbaijan
+B
+Belarus
+Belgium
+Bosnia and Herzegovina
+Bulgaria
+C
+Croatia
+Cyprus
+Czech Republic
+D
+Denmark
+E
+Estonia
+F
+Finland
+France
+G
+Georgia
+Germany
+Greece
+H
+Hungary
+I
+Iceland
+Ireland
+Italy
+K
+Kazakhstan
+Kosovo
+L
+Latvia
+Liechtenstein
+Lithuania
+Luxembourg
+M
+Macedonia (FYROM)
+Malta
+Moldova
+Monaco
+Montenegro
+N
+Netherlands
+Norway
+P
+Poland
+Portugal
+R
+Romania
+Russia
+S
+San Marino
+Serbia
+Slovakia
+Slovenia
+Spain
+Sweden
+Switzerland
+T
+Turkey
+U
+Ukraine
+United Kingdom (UK)
+V
+Vatican City
+</p>
+</div>
+<div class="zemynai-apacioje">
+	<ul class="zemynu-listas">
+		<li >
+		<img class="zemynu-foto" src="sumazinta-first-lady.jpg">
+				<a href="2"><p class="zemyno-pavadinimas">Antarctica</p></a>
+				<p class="mini-aprasymas">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+				do eiusmodtempor incididunt ut labore et dolore magna aliqua. 
+				consequat. </p>
+		</li>
+		<li>
+		<img class="zemynu-foto" src="sumazintas-benis-benasis.jpg">
+				<a href="2"><p class="zemyno-pavadinimas"> North America</p></a>
+				<p class="mini-aprasymas"> Duis aute irure dolor in reprehenderit in voluptate velit
+				esse cillum dolore eu fugiat nullapariatur.</p>
+		</li>
+		<li>
+		<img  class="zemynu-foto"src="sumazinta-second-lady.png">
+				<a href="2"><p class="zemyno-pavadinimas">South America</p></a>
+				<p class="aprasymai"> Ut enim ad minim veniam,
+				quis nostrud exercitation
+				ullamco laboris nisi ut aliquip
+				ex ea commodoconsequat.  
+				</p>
+		</li>
+		<li>
+		<img  class="zemynu-foto"src="sumazinta-second-lady.png">
+				<a href="2"><p class="zemyno-pavadinimas">Asia</p></a>
+				<p class="aprasymai"> Ut enim ad minim veniam,
+				quis nostrud exercitation
+				ullamco laboris nisi ut aliquip
+				ex ea commodoconsequat.  
+				</p>
+		</li>
+		<li>
+		<img  class="zemynu-foto"src="sumazinta-second-lady.png">
+				<a href="2"><p class="zemyno-pavadinimas">Australia</p></a>
+				<p class="aprasymai"> Ut enim ad minim veniam,
+				quis nostrud exercitation
+				ullamco laboris nisi ut aliquip
+				ex ea commodoconsequat.  
+				</p>
+				<p>For more information click <a href="2">here</a></p>
+		</li>
+</ul>
+</div>
+</div>
+</body>
+</html>
