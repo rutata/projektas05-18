b52b1826cd44e7d2899365eb94275e6898fb1bd6
rutata <raganqte@gmail.com>
2017-05-23 20:10:22 +0300

    daugiau failu pridejau

--- js/ruta.js
+++ js/ruta.js
@@ -1,15 +1,8 @@
  var options = [
-      {selector: '#staggered-test', offset: 50, callback: function(el) {
-        Materialize.toast("This is our ScrollFire Demo!", 1500 );
-      } },
-      {selector: '#staggered-test', offset: 205, callback: function(el) {
-        Materialize.toast("Please continue scrolling!", 1500 );
-      } },
       {selector: '#staggered-test', offset: 400, callback: function(el) {
         Materialize.showStaggeredList($(el));
-      } },
-      {selector: '#image-test', offset: 500, callback: function(el) {
-        Materialize.fadeInImage($(el));
       } }
     ];
+
+
     Materialize.scrollFire(options);
